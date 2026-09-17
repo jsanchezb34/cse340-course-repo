89145ec375dd69e51b15d92c9c92fd4f8f6b50e3
jsanchezb34 <jsanchezb34@>
2026-09-17 17:51:51 -0400

    learning activitiy Route and Query Parameters

--- src/models/projects.js
+++ src/models/projects.js
@@ -20,5 +20,26 @@ const getAllProjects = async () => {
   return result.rows;
 };
 
-export { getAllProjects };
+const getProjectsByOrganizationId = async (organizationId) => {
+      const query = `
+        SELECT
+          project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          project_date
+        FROM project
+        WHERE organization_id = $1
+        ORDER BY project_date;
+      `;
+      
+      const queryParams = [organizationId];
+      const result = await db.query(query, queryParams);
+
+      return result.rows;
+};
+
+export { getAllProjects, getProjectsByOrganizationId };
+
 
